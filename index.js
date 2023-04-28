3ced967defa8f6ecb860619c293191e548dd08d8
VictoriaAlekseeva <vicalekseeva88@gmail.com>
2023-04-28 20:36:02 +0200

    feat: add capslock handler and modules for keyboard layout

--- index.js
+++ index.js
@@ -1,301 +1,17 @@
-const keyboardEnKeys = {
-  "Backquote": "§",
-  "Digit1": "1",
-  "Digit2": "2",
-  "Digit3": "3",
-  "Digit4": "4",
-  "Digit5": "5",
-  "Digit6": "6",
-  "Digit7": "7",
-  "Digit8": "8",
-  "Digit9": "9",
-  "Digit0": "0",
-  "Minus": "-",
-  "Equal": "=",
-  "Backspace": "Backspace",
-  "Tab": "Tab",
-  "KeyQ": "q",
-  "KeyW": "w",
-  "KeyE": "e",
-  "KeyR": "r",
-  "KeyT": "t",
-  "KeyY": "y",
-  "KeyU": "u",
-  "KeyI": "i",
-  "KeyO": "o",
-  "KeyP": "p",
-  "BracketLeft": "[",
-  "BracketRight": "]",
-  "Backslash": "\\",
-  "Caps": "Caps",
-  "KeyA": "a",
-  "KeyS": "s",
-  "KeyD": "d",
-  "KeyF": "f",
-  "KeyG": "g",
-  "KeyH": "h",
-  "KeyJ": "j",
-  "KeyK": "k",
-  "KeyL": "l",
-  "Semicolon": ";",
-  "Quote": "'",
-  "Enter": "Enter",
-  "ShiftLeft": "Shift",
-  "IntlBackslash": "`",
-  "KeyZ": "z",
-  "KeyX": "x",
-  "KeyC": "c",
-  "KeyV": "v",
-  "KeyB": "b",
-  "KeyN": "n",
-  "KeyM": "m",
-  "Comma": ",",
-  "Period": ".",
-  "Slash": "/",
-  "ArrowUp": "&#9650;",
-  "ShiftRight": "Shift",
-  "Fn": "Fn",
-  "ControlLeft": "Ctrl",
-  "LeftOpt": "Opt",
-  "LeftCmd": "Cmd",
-  "Space": " ",
-  "RightCmd": "Cmd",
-  "RightOpt": "Opt",
-  "ArrowLeft": "&#9668;",
-  "ArrowDown": "&#9660;",
-  "ArrowRight": "&#9658;"
-};
+import keyboardEnCapsKeys from './src/en/keyboardEnCapsKeys.js';
+import keyboardEnCapsShiftKeys from './src/en/keyboardEnCapsShiftKeys.js';
+import keyboardEnKeys from './src/en/keyboardEnKeys.js';
+import keyboardEnUpperCaseKeys from './src/en/keyboardEnUpperCaseKeys.js';
+import keyboardRuCapsKeys from './src/ru/keyboardRuCapsKeys.js';
+import keyboardRuCapsShiftKeys from './src/ru/keyboardRuCapsShiftKeys.js';
+import keyboardRuKeys from './src/ru/keyboardRuKeys.js';
+import keyboardRuUpperCaseKeys from './src/ru/keyboardRuUpperCaseKeys.js';
 
-const keyboardEnUpperCaseKeys = {
-  "Backquote": "±",
-  "Digit1": "!",
-  "Digit2": "@",
-  "Digit3": "#",
-  "Digit4": "$",
-  "Digit5": "%",
-  "Digit6": "^",
-  "Digit7": "&",
-  "Digit8": "*",
-  "Digit9": "(",
-  "Digit0": ")",
-  "Minus": "_",
-  "Equal": "+",
-  "Backspace": "Backspace",
-  "Tab": "Tab",
-  "KeyQ": "Q",
-  "KeyW": "W",
-  "KeyE": "E",
-  "KeyR": "R",
-  "KeyT": "T",
-  "KeyY": "Y",
-  "KeyU": "U",
-  "KeyI": "I",
-  "KeyO": "O",
-  "KeyP": "P",
-  "BracketLeft": "{",
-  "BracketRight": "}",
-  "Backslash": "|",
-  "Caps": "Caps",
-  "KeyA": "A",
-  "KeyS": "S",
-  "KeyD": "D",
-  "KeyF": "F",
-  "KeyG": "G",
-  "KeyH": "H",
-  "KeyJ": "J",
-  "KeyK": "K",
-  "KeyL": "L",
-  "Semicolon": ":",
-  "Quote": "\"",
-  "Enter": "Enter",
-  "ShiftLeft": "Shift",
-  "IntlBackslash": "~",
-  "KeyZ": "Z",
-  "KeyX": "X",
-  "KeyC": "C",
-  "KeyV": "V",
-  "KeyB": "B",
-  "KeyN": "N",
-  "KeyM": "M",
-  "Comma": "<",
-  "Period": ">",
-  "Slash": "?",
-  "ArrowUp": "&#9650;",
-  "ShiftRight": "Shift",
-  "Fn": "Fn",
-  "ControlLeft": "Ctrl",
-  "LeftOpt": "Opt",
-  "LeftCmd": "Cmd",
-  "Space": " ",
-  "RightCmd": "Cmd",
-  "RightOpt": "Opt",
-  "ArrowLeft": "&#9668;",
-  "ArrowDown": "&#9660;",
-  "ArrowRight": "&#9658;"
-}
-
-const keyboardEnCapsKeys = {
-  "Backquote": "§",
-  "Digit1": "1",
-  "Digit2": "2",
-  "Digit3": "3",
-  "Digit4": "4",
-  "Digit5": "5",
-  "Digit6": "6",
-  "Digit7": "7",
-  "Digit8": "8",
-  "Digit9": "9",
-  "Digit0": "0",
-  "Minus": "-",
-  "Equal": "=",
-  "Backspace": "Backspace",
-  "Tab": "Tab",
-  "KeyQ": "Q",
-  "KeyW": "W",
-  "KeyE": "E",
-  "KeyR": "R",
-  "KeyT": "T",
-  "KeyY": "Y",
-  "KeyU": "U",
-  "KeyI": "I",
-  "KeyO": "O",
-  "KeyP": "P",
-  "BracketLeft": "[",
-  "BracketRight": "]",
-  "Backslash": "\\",
-  "Caps": "Caps",
-  "KeyA": "A",
-  "KeyS": "S",
-  "KeyD": "D",
-  "KeyF": "F",
-  "KeyG": "G",
-  "KeyH": "H",
-  "KeyJ": "J",
-  "KeyK": "K",
-  "KeyL": "L",
-  "Semicolon": ";",
-  "Quote": "'",
-  "Enter": "Enter",
-  "ShiftLeft": "Shift",
-  "IntlBackslash": "`",
-  "KeyZ": "Z",
-  "KeyX": "X",
-  "KeyC": "C",
-  "KeyV": "V",
-  "KeyB": "B",
-  "KeyN": "N",
-  "KeyM": "M",
-  "Comma": ",",
-  "Period": ".",
-  "Slash": "/",
-  "ArrowUp": "&#9650;",
-  "ShiftRight": "Shift",
-  "Fn": "Fn",
-  "ControlLeft": "Ctrl",
-  "LeftOpt": "Opt",
-  "LeftCmd": "Cmd",
-  "Space": " ",
-  "RightCmd": "Cmd",
-  "RightOpt": "Opt",
-  "ArrowLeft": "&#9668;",
-  "ArrowDown": "&#9660;",
-  "ArrowRight": "&#9658;"
-}
-
-const keyboardEnCapsShiftKeys = {
-  "Backquote": "±",
-  "Digit1": "!",
-  "Digit2": "@",
-  "Digit3": "#",
-  "Digit4": "$",
-  "Digit5": "%",
-  "Digit6": "^",
-  "Digit7": "&",
-  "Digit8": "*",
-  "Digit9": "(",
-  "Digit0": ")",
-  "Minus": "_",
-  "Equal": "+",
-  "Backspace": "Backspace",
-  "Tab": "Tab",
-  "KeyQ": "Q",
-  "KeyW": "W",
-  "KeyE": "E",
-  "KeyR": "R",
-  "KeyT": "T",
-  "KeyY": "Y",
-  "KeyU": "U",
-  "KeyI": "I",
-  "KeyO": "O",
-  "KeyP": "P",
-  "BracketLeft": "{",
-  "BracketRight": "}",
-  "Backslash": "|",
-  "Caps": "Caps",
-  "KeyA": "A",
-  "KeyS": "S",
-  "KeyD": "D",
-  "KeyF": "F",
-  "KeyG": "G",
-  "KeyH": "H",
-  "KeyJ": "J",
-  "KeyK": "K",
-  "KeyL": "L",
-  "Semicolon": ":",
-  "Quote": "\"",
-  "Enter": "Enter",
-  "ShiftLeft": "Shift",
-  "IntlBackslash": "~",
-  "KeyZ": "Z",
-  "KeyX": "X",
-  "KeyC": "C",
-  "KeyV": "V",
-  "KeyB": "B",
-  "KeyN": "N",
-  "KeyM": "M",
-  "Comma": "<",
-  "Period": ">",
-  "Slash": "?",
-  "ArrowUp": "&#9650;",
-  "ShiftRight": "Shift",
-  "Fn": "Fn",
-  "ControlLeft": "Ctrl",
-  "LeftOpt": "Opt",
-  "LeftCmd": "Cmd",
-  "Space": " ",
-  "RightCmd": "Cmd",
-  "RightOpt": "Opt",
-  "ArrowLeft": "&#9668;",
-  "ArrowDown": "&#9660;",
-  "ArrowRight": "&#9658;"
-}
 
 const keyboardEnKeysArray = Object.keys(keyboardEnKeys);
 const keyboardEnUpperCaseKeysArray = Object.keys(keyboardEnUpperCaseKeys);
 const keyboardEnCapsKeysArray = Object.keys(keyboardEnCapsKeys);
 const keyboardEnCapsShiftKeysArray = Object.keys(keyboardEnCapsShiftKeys);
-
-// генерация объектов для заполнения клавиатуры по нажатию клавиш
-// document.onkeypress = function(event) {
-
-//   console.log('keyboardEn', Object.keys(keyboardEn).length)
-//   console.log ('keyboardEnCapsShiftKeys', Object.keys(keyboardEnCapsShiftKeys).length)
-//   console.log ('keyboardEnCapsShift', Object.keys(keyboardEnCapsShift).length)
-//   console.log ('keyboardEnCapsKeys', Object.keys(keyboardEnCapsKeys).length)
-//   console.log ('keyboardEnCaps', Object.keys(keyboardEnCaps).length)
-//   console.log ('keyboardEnUpperCaseKeys', Object.keys(keyboardEnUpperCaseKeys).length)
-//   console.log ('keyboardEnUpperCase', Object.keys(keyboardEnUpperCase).length)
-//   console.log ('keyboardEnKeys', Object.keys(keyboardEnKeys).length)
-//   console.log ('keyboardEn', Object.keys(keyboardEn).length)
-
-//   console.log(event)
-
-//   // keyboardEnCapsShift[event.key] = event.code;
-//   // keyboardEnCapsShiftKeys[event.code] = event.key;
-
-//   // console.log('keyboardEnCapsShift', keyboardEnCapsShift);
-//   // console.log('keyboardEnCapsShiftKeys', keyboardEnCapsShiftKeys)
-// }
 
 // HTML markup generation
 
@@ -321,65 +37,76 @@ const keyboardEnCapsShiftKeysArray = Object.keys(keyboardEnCapsShiftKeys);
 //   return this.createElem;
 // }
 
-const keyboard = document.createElement('div');
-const textarea = document.createElement('textarea');
+
+
+// class NewElem {
+//   constructor (element, className) {
+//     this.element = element;
+//     this.className = className;
+//   }
+
+//   createElement() {
+//     const el = document.createElement(this.element);
+//     el.className = this.className;
+//         return el;
+//   }
+// }
+
+function CreateElem(tag, clName) {
+  this.tag = tag;
+  this.clName = clName;
+
+  this.createBlock = document.createElement(tag);
+  this.createBlock.className = clName;
+
+  return this.createBlock;
+}
+
+const keyboard = new CreateElem('div', 'keyboard');
+const textarea = new CreateElem('textarea', 'textarea');
+textarea.id = 'textarea';
+textarea.rows = 5;
+textarea.cols = 50;
 
 function generateLayout() {
   const headertext = 'RSS Виртуальная клавиатура';
-  const keyboarsRowsLength = [14, 14, 13, 14, 10]
+  const keyboarsRowsLength = [14, 14, 13, 14, 10]; // количество кнопок в каждом ряду
 
-  const wrapper = document.createElement('div');
-  wrapper.className = 'wrapper';
+  const wrapper = new CreateElem('div', 'wrapper');
   document.body.prepend(wrapper);
 
-  const header = document.createElement('h1');
-  header.className = "h1";
+  const header = new CreateElem('h1', 'h1');
   header.innerHTML = headertext;
   wrapper.append(header);
 
-  // const textarea = document.createElement('textarea');
-  textarea.id = 'textarea';
-  textarea.className = 'textarea';
-  textarea.rows = 5;
-  textarea.cols = 50;
   wrapper.append(textarea);
-
-  // const keyboard = document.createElement('div');
-  keyboard.className = 'keyboard';
   wrapper.append(keyboard);
 
   let shift = 0;
 
   for (let i = 0; i < keyboarsRowsLength.length; i++) {
-    let row = document.createElement('div');
-    row.className = `row row${i+1}`;
+    let row = new CreateElem('div', `row row${i+1}`);
     keyboard.append(row);
 
     for (let j = 0; j < keyboarsRowsLength[i]; j++) {
-      let keyboardButton = document.createElement('div');
-      keyboardButton.className = `key ${keyboardEnKeysArray[shift+j]}`;
+      let keyboardButton = new CreateElem('div', `key ${keyboardEnKeysArray[shift+j]}`);
       row.append(keyboardButton);
 
-      let caseDown = document.createElement('span');
-      caseDown.className = 'caseDown';
+      let caseDown = new CreateElem('span', 'caseDown');
       caseDown.innerHTML = keyboardEnKeys[keyboardEnKeysArray[shift+j]];
       keyboardButton.append(caseDown);
 
-      let caseUp = document.createElement('span');
-      caseUp.className = 'caseUp hidden';
+      let caseUp = new CreateElem('span', 'caseUp hidden');
       caseUp.innerHTML = keyboardEnUpperCaseKeys[keyboardEnUpperCaseKeysArray[shift+j]];
       keyboardButton.append(caseUp);
 
-      let caps = document.createElement('span');
-      caps.className = 'caps hidden';
+      let caps = new CreateElem('span', 'caseUp caps hidden');
       caps.innerHTML = keyboardEnCapsKeys[keyboardEnCapsKeysArray[shift+j]];
       keyboardButton.append(caps);
 
-      let capsShift = document.createElement('span');
-      capsShift.className = 'capsShift hidden';
+      let capsShift = new CreateElem('span', 'capsShift hidden');
       capsShift.innerHTML = keyboardEnCapsShiftKeys[keyboardEnCapsShiftKeysArray[shift+j]];
       keyboardButton.append(capsShift);
-      //! нужна функция генератор кнопок
     }
     shift += keyboarsRowsLength[i];
   }
@@ -395,6 +122,34 @@ keyboard.onclick = function mouseDownHandler(event) {
 
   if (!target) return;
   if (!keyboard.contains(target)) return;
+
+  // регистр с капсом
+  if (target.classList.contains('CapsLock')) {
+    target.classList.toggle('active');
+    const keys = document.querySelectorAll('.key > *');
+
+    if (target.classList.contains('active')) {
+      for (let key of keys) {
+        console.log(key.classList.contains('hidden'));
+        if (!key.classList.contains('hidden')) {
+          key.classList.add('hidden')
+        }
+        if (key.classList.contains('caps')) {
+          key.classList.remove('hidden');
+        }
+      }
+    } else {
+      for (let key of keys) {
+        console.log(key.classList.contains('caps'));
+        if (!key.classList.contains('hidden')) {
+          key.classList.add('hidden')
+        }
+        if (key.classList.contains('caseDown')) {
+          key.classList.remove('hidden');
+        }
+      }
+    }
+  }
 
   // chooseButton(target);
 
---
+++ src/en/en.js
@@ -0,0 +1,10 @@
+
+
+const keyboardEn = {
+  "keyboardEnKeys": keyboardEnKeys,
+  "keyboardEnUpperCaseKeys": keyboardEnUpperCaseKeys,
+  "keyboardEnCapsKeys": keyboardEnCapsKeys,
+  "keyboardEnCapsShiftKeys": keyboardEnCapsShiftKeys
+}
+
+console.log(keyboardEn);
