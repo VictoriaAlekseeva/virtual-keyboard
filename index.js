55a24a646dfad3d37579a887084465eea87e95e8
VictoriaAlekseeva <vicalekseeva88@gmail.com>
2023-04-29 16:30:09 +0200

    refactor: add modules, export/import for modules

--- index.js
+++ index.js
@@ -1,171 +1,173 @@
-import keyboardEnCapsKeys from './src/en/keyboardEnCapsKeys.js';
-import keyboardEnCapsShiftKeys from './src/en/keyboardEnCapsShiftKeys.js';
-import keyboardEnKeys from './src/en/keyboardEnKeys.js';
-import keyboardEnUpperCaseKeys from './src/en/keyboardEnUpperCaseKeys.js';
-import keyboardRuCapsKeys from './src/ru/keyboardRuCapsKeys.js';
-import keyboardRuCapsShiftKeys from './src/ru/keyboardRuCapsShiftKeys.js';
-import keyboardRuKeys from './src/ru/keyboardRuKeys.js';
-import keyboardRuUpperCaseKeys from './src/ru/keyboardRuUpperCaseKeys.js';
+import { generateLayout } from './src/modules/generateLayout.js';
 
-
-const keyboardEnKeysArray = Object.keys(keyboardEnKeys);
-const keyboardEnUpperCaseKeysArray = Object.keys(keyboardEnUpperCaseKeys);
-const keyboardEnCapsKeysArray = Object.keys(keyboardEnCapsKeys);
-const keyboardEnCapsShiftKeysArray = Object.keys(keyboardEnCapsShiftKeys);
-
-// HTML markup generation
-
-// function createBlock(tag, clName) {
-//   this.tag = tag;
-//   this.clName = clName;
-
-//   this.createBlock = document.createElement(tag);
-//   this.createBlock.className = clName;
-
-//   return this.createBlock;
-// }
-
-// function createElem (tag, clName, content) {
-//   this.tag = tag;
-//   this.clName = clName;
-//   this.content = content;
-
-//   this.createElem = document.createElement(tag);
-//   this.createElem.className = clName;
-//   this.createElem.innerHTML = content;
-
-//   return this.createElem;
-// }
-
-
-
-// class NewElem {
-//   constructor (element, className) {
-//     this.element = element;
-//     this.className = className;
-//   }
-
-//   createElement() {
-//     const el = document.createElement(this.element);
-//     el.className = this.className;
-//         return el;
-//   }
-// }
-
-function CreateElem(tag, clName) {
-  this.tag = tag;
-  this.clName = clName;
-
-  this.createBlock = document.createElement(tag);
-  this.createBlock.className = clName;
-
-  return this.createBlock;
+window.onload = function() {
+  generateLayout();
 }
 
-const keyboard = new CreateElem('div', 'keyboard');
-const textarea = new CreateElem('textarea', 'textarea');
-textarea.id = 'textarea';
-textarea.rows = 5;
-textarea.cols = 50;
-
-function generateLayout() {
-  const headertext = 'RSS Виртуальная клавиатура';
-  const keyboarsRowsLength = [14, 14, 13, 14, 10]; // количество кнопок в каждом ряду
-
-  const wrapper = new CreateElem('div', 'wrapper');
-  document.body.prepend(wrapper);
-
-  const header = new CreateElem('h1', 'h1');
-  header.innerHTML = headertext;
-  wrapper.append(header);
-
-  wrapper.append(textarea);
-  wrapper.append(keyboard);
-
-  let shift = 0;
-
-  for (let i = 0; i < keyboarsRowsLength.length; i++) {
-    let row = new CreateElem('div', `row row${i+1}`);
-    keyboard.append(row);
-
-    for (let j = 0; j < keyboarsRowsLength[i]; j++) {
-      let keyboardButton = new CreateElem('div', `key ${keyboardEnKeysArray[shift+j]}`);
-      row.append(keyboardButton);
-
-      let caseDown = new CreateElem('span', 'caseDown');
-      caseDown.innerHTML = keyboardEnKeys[keyboardEnKeysArray[shift+j]];
-      keyboardButton.append(caseDown);
-
-      let caseUp = new CreateElem('span', 'caseUp hidden');
-      caseUp.innerHTML = keyboardEnUpperCaseKeys[keyboardEnUpperCaseKeysArray[shift+j]];
-      keyboardButton.append(caseUp);
-
-      let caps = new CreateElem('span', 'caseUp caps hidden');
-      caps.innerHTML = keyboardEnCapsKeys[keyboardEnCapsKeysArray[shift+j]];
-      keyboardButton.append(caps);
-
-      let capsShift = new CreateElem('span', 'capsShift hidden');
-      capsShift.innerHTML = keyboardEnCapsShiftKeys[keyboardEnCapsShiftKeysArray[shift+j]];
-      keyboardButton.append(capsShift);
-    }
-    shift += keyboarsRowsLength[i];
-  }
-}
-
-generateLayout();
+const keyboard = document.querySelector('.keyboard');
+const textarea = document.querySelector('.textarea');
 
 let textAreaText = '';
 
-keyboard.onclick = function mouseDownHandler(event) {
+const caseDown = document.querySelectorAll('.caseDown');
+const caseUp = document.querySelectorAll('.caseUp');
+const caps = document.querySelectorAll('.caps');
+const capsShift = document.querySelectorAll('.capsShift');
+
+keyboard.addEventListener('click', function(event){
   const target = event.target.closest('div.key');
-  console.log(target)
+  console.log(target);
 
   if (!target) return;
   if (!keyboard.contains(target)) return;
 
   // регистр с капсом
-  if (target.classList.contains('CapsLock')) {
-    target.classList.toggle('active');
-    const keys = document.querySelectorAll('.key > *');
-
-    if (target.classList.contains('active')) {
-      for (let key of keys) {
-        console.log(key.classList.contains('hidden'));
-        if (!key.classList.contains('hidden')) {
-          key.classList.add('hidden')
-        }
-        if (key.classList.contains('caps')) {
-          key.classList.remove('hidden');
-        }
-      }
-    } else {
-      for (let key of keys) {
-        console.log(key.classList.contains('caps'));
-        if (!key.classList.contains('hidden')) {
-          key.classList.add('hidden')
-        }
-        if (key.classList.contains('caseDown')) {
-          key.classList.remove('hidden');
-        }
-      }
-    }
-  }
-
-  // chooseButton(target);
-
-  console.log(target.className);
+  capsLockHandler(target);
 
   const letterCode = target.className.split(' ')[1];
   textAreaText += keyboardEnKeys[letterCode];
   textarea.innerHTML = textAreaText;
+})
+
+function capsLockHandler(target)   {
+  if (target.classList.contains('CapsLock')) {
+    target.classList.toggle('active');
+
+    if (target.classList.contains('active')) {
+      caseDown.forEach(el => {
+        el.classList.remove('active');
+        el.classList.add('hidden');
+      });
+
+      caseUp.forEach(el => {
+        el.classList.remove('active');
+        el.classList.add('hidden');
+      });
+
+      capsShift.forEach(el => {
+        el.classList.remove('active');
+        el.classList.add('hidden');
+      });
+
+      caps.forEach(el => {
+        el.classList.remove('hidden');
+        el.classList.add('active');
+      });} else {
+      caseDown.forEach(el => {
+        el.classList.add('active');
+        el.classList.remove('hidden');
+      });
+
+      caseUp.forEach(el => {
+        el.classList.add('hidden');
+      });
+
+      capsShift.forEach(el => {
+        el.classList.add('hidden');
+      });
+
+      caps.forEach(el => {
+        el.classList.add('hidden');
+      });
+    }
+  }
 }
 
-// let selectedTD;
-// function chooseButton(td) {
-//   if (selectedTD) {
-//     selectedTD.classList.remove('active');
-//   }
+keyboard.addEventListener('mousedown', function (event) {
+  const target = event.target.closest('div.key');
+  toUpperCase(target)
+})
 
-//   selectedTD = td;
-//   selectedTD.classList.add('active');
-// }
+keyboard.addEventListener('mouseup', function (event) {
+  const target = event.target.closest('div.key');
+  toLowerCase(target)
+})
+
+// keyboard.addEventListener('mouseup', function(event){
+
+// });
+
+document.addEventListener('keydown', function(event){
+  console.log(event)
+});
+
+// document.addEventListener('keyup', function(event){
+
+// });
+
+function toUpperCase(target) {
+  if (target.classList.contains('ShiftLeft') || target.classList.contains('ShiftRight')) {
+    target.classList.add('active');
+
+    if (target.classList.contains('active')) {
+      caseDown.forEach(el => {
+          el.classList.remove('active');
+          el.classList.add('hidden');
+      });
+
+      caseUp.forEach(el => {
+        el.classList.remove('active');
+        el.classList.add('hidden');
+      });
+
+      capsShift.forEach(el => {
+        el.classList.remove('active');
+        el.classList.add('hidden');
+    });
+
+      caseUp.forEach(el => {
+        el.classList.remove('hidden');
+        el.classList.add('active');
+    });
+    } else {
+      caseUp.forEach(el => {
+        el.classList.add('active');
+        el.classList.remove('hidden');
+    });
+
+    caseUp.forEach(el => {
+      el.classList.add('hidden');
+    });
+
+    capsShift.forEach(el => {
+      el.classList.add('hidden');
+    });
+
+    caps.forEach(el => {
+      el.classList.add('hidden');
+    });
+    }
+  }
+}
+
+function toLowerCase(target) {
+  if (target.classList.contains('ShiftLeft') || target.classList.contains('ShiftRight')) {
+    target.classList.remove('active');
+
+    if (!target.classList.contains('active')) {
+      caseDown.forEach(el => {
+        el.classList.add('active');
+        el.classList.remove('hidden');
+      });
+
+      caseUp.forEach(el => {
+        el.classList.remove('active');
+        el.classList.add('hidden');
+      });
+
+      capsShift.forEach(el => {
+        el.classList.remove('active');
+        el.classList.add('hidden');
+    });
+
+      caseUp.forEach(el => {
+        el.classList.add('hidden');
+        el.classList.remove('active');
+    });
+    }
+  }
+}
+
+function changeLanguage() {
+
+}
---
+++ src/modules/generateLayout.js
@@ -0,0 +1,92 @@
+import { CreateElem } from "./createElement.js";
+
+import keyboardEnCapsKeys from '../languages/en/keyboardEnCapsKeys.js';
+import keyboardEnCapsShiftKeys from '../languages/en/keyboardEnCapsShiftKeys.js';
+import keyboardEnKeys from '../languages/en/keyboardEnKeys.js';
+import keyboardEnUpperCaseKeys from '../languages/en/keyboardEnUpperCaseKeys.js';
+import keyboardRuCapsKeys from '../languages/ru/keyboardRuCapsKeys.js';
+import keyboardRuCapsShiftKeys from '../languages/ru/keyboardRuCapsShiftKeys.js';
+import keyboardRuKeys from '../languages/ru/keyboardRuKeys.js';
+import keyboardRuUpperCaseKeys from '../languages/ru/keyboardRuUpperCaseKeys.js';
+
+const keyboardEnKeysArray = Object.keys(keyboardEnKeys);
+const keyboardEnUpperCaseKeysArray = Object.keys(keyboardEnUpperCaseKeys);
+const keyboardEnCapsKeysArray = Object.keys(keyboardEnCapsKeys);
+const keyboardEnCapsShiftKeysArray = Object.keys(keyboardEnCapsShiftKeys);
+const keyboardRuKeysArray = Object.keys(keyboardRuKeys);
+const keyboardRuUpperCaseKeysArray = Object.keys(keyboardRuUpperCaseKeys);
+const keyboardRuCapsKeysArray = Object.keys(keyboardRuCapsKeys);
+const keyboardRuCapsShiftKeysArray = Object.keys(keyboardRuCapsShiftKeys);
+
+
+export function generateLayout() {
+  const headertext = 'RSS Виртуальная клавиатура';
+  const keyboarsRowsLength = [14, 14, 13, 14, 10]; // количество кнопок в каждом ряду
+
+  const wrapper = new CreateElem('div', 'wrapper');
+  document.body.prepend(wrapper);
+
+  const header = new CreateElem('h1', 'h1');
+  header.innerHTML = headertext;
+  wrapper.append(header);
+
+
+  const keyboard = new CreateElem('div', 'keyboard');
+  const textarea = new CreateElem('textarea', 'textarea');
+  textarea.id = 'textarea';
+  textarea.rows = 5;
+  textarea.cols = 50;
+  wrapper.append(textarea);
+  wrapper.append(keyboard);
+
+  let shift = 0;
+
+  for (let i = 0; i < keyboarsRowsLength.length; i++) {
+    let row = new CreateElem('div', `row row${i+1}`);
+    keyboard.append(row);
+
+    for (let j = 0; j < keyboarsRowsLength[i]; j++) {
+      let keyboardButton = new CreateElem('div', `key ${keyboardEnKeysArray[shift+j]}`);
+      row.append(keyboardButton);
+
+      let ruButton = new CreateElem('span', 'ru hidden');
+      keyboardButton.append(ruButton);
+
+      let ruCaseDown = new CreateElem('span', 'caseDown active');
+      ruCaseDown.innerHTML = keyboardEnKeys[keyboardRuKeysArray[shift+j]];
+      ruButton.append(ruCaseDown);
+
+      let ruCaseUp = new CreateElem('span', 'caseUp hidden');
+      ruCaseUp.innerHTML = keyboardEnUpperCaseKeys[keyboardRuUpperCaseKeysArray[shift+j]];
+      ruButton.append(ruCaseUp);
+
+      let ruCaps = new CreateElem('span', 'caps hidden');
+      ruCaps.innerHTML = keyboardEnCapsKeys[keyboardRuCapsKeysArray[shift+j]];
+      ruButton.append(ruCaps);
+
+      let ruCapsShift = new CreateElem('span', 'capsShift hidden');
+      ruCapsShift.innerHTML = keyboardEnCapsShiftKeys[keyboardRuCapsShiftKeysArray[shift+j]];
+      ruButton.append(ruCapsShift);
+
+      let enButton = new CreateElem('span', 'en');
+      keyboardButton.append(enButton);
+
+      let enCaseDown = new CreateElem('span', 'caseDown active');
+      enCaseDown.innerHTML = keyboardEnKeys[keyboardEnKeysArray[shift+j]];
+      enButton.append(enCaseDown);
+
+      let enCaseUp = new CreateElem('span', 'caseUp hidden');
+      enCaseUp.innerHTML = keyboardEnUpperCaseKeys[keyboardEnUpperCaseKeysArray[shift+j]];
+      enButton.append(enCaseUp);
+
+      let enCaps = new CreateElem('span', 'caps hidden');
+      enCaps.innerHTML = keyboardEnCapsKeys[keyboardEnCapsKeysArray[shift+j]];
+      enButton.append(enCaps);
+
+      let enCapsShift = new CreateElem('span', 'capsShift hidden');
+      enCapsShift.innerHTML = keyboardEnCapsShiftKeys[keyboardEnCapsShiftKeysArray[shift+j]];
+      enButton.append(enCapsShift);
+    }
+    shift += keyboarsRowsLength[i];
+  }
+}
