ad02123bda0192d49a4a413dd87e55b52006e912
VictoriaAlekseeva <vicalekseeva88@gmail.com>
2023-05-01 00:17:07 +0200

    refactor: move capsLockOnOff to separate module

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ import setLanguage from './src/modules/setLanguage.js';
 
 import toUpperCase from './src/modules/toUpperCase.js';
 import toLowerCase from './src/modules/toLowerCase.js';
+import capsLockOnOff from './src/modules/toCapsLock.js';
 
 let language = localStorage.getItem('language');
 language = language == null ? 'en' : language;
@@ -106,63 +107,63 @@ function textType(target, code) {
   }
 }
 
-function capsLockOnOff(event, target) {
-  if (event.getModifierState('CapsLock')) {
-    target.classList.add('active');
-    caseDown.forEach((el) => {
-      el.classList.remove('active');
-      el.classList.add('hidden');
-    });
+// function capsLockOnOff(event, target) {
+//   if (event.getModifierState('CapsLock')) {
+//     target.classList.add('active');
+//     caseDown.forEach((el) => {
+//       el.classList.remove('active');
+//       el.classList.add('hidden');
+//     });
 
-    caps.forEach((el) => {
-      el.classList.add('active');
-      el.classList.remove('hidden');
-    });
-  } else {
-    target.classList.remove('active');
-    caseDown.forEach((el) => {
-      el.classList.add('active');
-      el.classList.remove('hidden');
-    });
+//     caps.forEach((el) => {
+//       el.classList.add('active');
+//       el.classList.remove('hidden');
+//     });
+//   } else {
+//     target.classList.remove('active');
+//     caseDown.forEach((el) => {
+//       el.classList.add('active');
+//       el.classList.remove('hidden');
+//     });
 
-    caps.forEach((el) => {
-      el.classList.remove('active');
-      el.classList.add('hidden');
-    });
-  }
-}
+//     caps.forEach((el) => {
+//       el.classList.remove('active');
+//       el.classList.add('hidden');
+//     });
+//   }
+// }
 
-function capsLockHandler(target) {
-  if ((letterCase !== 'capsShift') && (target.classList.contains('CapsLock'))) {
-    target.classList.toggle('active');
+// function capsLockHandler(target) {
+  // if ((letterCase !== 'capsShift') && (target.classList.contains('CapsLock'))) {
+  //   target.classList.toggle('active');
 
-    letterCase = 'caps';
+  //   letterCase = 'caps';
 
-    if (target.classList.contains('active')) {
-      letterCase = 'caps';
-      caseDown.forEach((el) => {
-        el.classList.remove('active');
-        el.classList.add('hidden');
-      });
+  //   if (target.classList.contains('active')) {
+  //     letterCase = 'caps';
+  //     caseDown.forEach((el) => {
+  //       el.classList.remove('active');
+  //       el.classList.add('hidden');
+  //     });
 
-      caps.forEach((el) => {
-        el.classList.add('active');
-        el.classList.remove('hidden');
-      });
-    } else if (!target.classList.contains('active')) {
-      letterCase = 'caseDown';
-      caseDown.forEach((el) => {
-        el.classList.add('active');
-        el.classList.remove('hidden');
-      });
+  //     caps.forEach((el) => {
+  //       el.classList.add('active');
+  //       el.classList.remove('hidden');
+  //     });
+  //   } else if (!target.classList.contains('active')) {
+  //     letterCase = 'caseDown';
+  //     caseDown.forEach((el) => {
+  //       el.classList.add('active');
+  //       el.classList.remove('hidden');
+  //     });
 
-      caps.forEach((el) => {
-        el.classList.remove('active');
-        el.classList.add('hidden');
-      });
-    }
-  }
-}
+  //     caps.forEach((el) => {
+  //       el.classList.remove('active');
+  //       el.classList.add('hidden');
+  //     });
+  //   }
+  // }
+// }
 
 setLanguage(language, letterCase);
 
@@ -170,7 +171,7 @@ window.addEventListener('keydown', (event) => {
   const target = document.querySelector(`.${event.code}`);
   target.classList.add('active');
   textType(target, event.code);
-  capsLockOnOff(event, target);
+  capsLockOnOff(event, target, letterCase, language);
   toUpperCase(target, letterCase, language);
   shiftCapsMouseDown(target);
 });
@@ -178,7 +179,7 @@ window.addEventListener('keydown', (event) => {
 window.addEventListener('keyup', (event) => {
   const target = document.querySelector(`.${event.code}`);
   target.classList.remove('active');
-  capsLockOnOff(event, target);
+  capsLockOnOff(event, target, letterCase, language);
   shiftCapsMouseUp(target);
   toLowerCase(target, letterCase, language);
 });
---
+++ src/modules/toCapsLock.js
@@ -0,0 +1,22 @@
+export default function capsLockOnOff(event, target, letterCase, language) {
+  const activeLanguage = document.querySelectorAll(`.${language}.active`)
+
+    if (event.getModifierState('CapsLock') && !event.getModifierState('Shift')) {
+      target.classList.add('active');
+      letterCase = 'caps';
+      activeLanguage.forEach(el => {
+        el.querySelector('.caseDown').classList.remove('active');
+        el.querySelector('.caseDown').classList.add('hidden');
+        el.querySelector('.caps').classList.add('active');
+        el.querySelector('.caps').classList.remove('hidden');
+      })
+    } else {
+      target.classList.remove('active');
+      activeLanguage.forEach(el => {
+        el.querySelector('.caseDown').classList.add('active');
+        el.querySelector('.caseDown').classList.remove('hidden');
+        el.querySelector('.caps').classList.remove('active');
+        el.querySelector('.caps').classList.add('hidden');
+      })
+    }
+}
