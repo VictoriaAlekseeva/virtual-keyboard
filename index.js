8c996022d59bcb9ef6b28c1f3687a4b442405422
VictoriaAlekseeva <vicalekseeva88@gmail.com>
2023-05-04 18:37:18 +0200

    fix: service buttons work with clicks

--- index.js
+++ index.js
@@ -138,25 +138,40 @@ function textType(target, event) {
     if (!event.key) {
       switch (letter.textContent) {
         case '>':
-        textarea.value = `${value.slice(0, start)}>${value.slice(end, value.length)}`;
-        textarea.selectionStart = start + 1;
-        textarea.selectionEnd = end + 1;
-        break;
-      case '<':
-        textarea.value = `${value.slice(0, start)}<${value.slice(end, value.length)}`;
-        textarea.selectionStart = start + 1;
-        textarea.selectionEnd = end + 1;
-        break;
-      case '&':
-        textarea.value = `${value.slice(0, start)}&${value.slice(end, value.length)}`.replace(/&amp;/g, '&');
-        textarea.selectionStart = start + 1;
-        textarea.selectionEnd = end + 1;
-        console.log('!!!')
-        break;
-      default:
-        textarea.value = `${value.slice(0, start)}${letter.innerHTML}${value.slice(end, value.length)}`;
-        textarea.selectionStart = start + 1;
-        textarea.selectionEnd = end + 1;
+          textarea.value = `${value.slice(0, start)}>${value.slice(end, value.length)}`;
+          textarea.selectionStart = start + 1;
+          textarea.selectionEnd = end + 1;
+          break;
+        case '<':
+          textarea.value = `${value.slice(0, start)}<${value.slice(end, value.length)}`;
+          textarea.selectionStart = start + 1;
+          textarea.selectionEnd = end + 1;
+          break;
+        case '&':
+          textarea.value = `${value.slice(0, start)}&${value.slice(end, value.length)}`.replace(/&amp;/g, '&');
+          textarea.selectionStart = start + 1;
+          textarea.selectionEnd = end + 1;
+          break;
+        case 'Tab':
+          textarea.value = `${value.slice(0, start)}\t${value.slice(end, value.length)}`;
+          textarea.selectionStart = start + 1;
+          textarea.selectionEnd = end + 1;
+          break;
+        case 'Enter':
+          textarea.value = `${value.slice(0, start)}\n${value.slice(end, value.length)}`;
+          textarea.selectionStart = start + 1;
+          textarea.selectionEnd = end + 1;
+          break;
+        case 'Backspace':
+          if (start === 0) break;
+          textarea.value = `${value.slice(0, start - 1)}${value.slice(end, value.length)}`;
+          textarea.selectionStart = start;
+          textarea.selectionEnd = end - 1;
+          break;
+        default:
+          textarea.value = `${value.slice(0, start)}${letter.innerHTML}${value.slice(end, value.length)}`;
+          textarea.selectionStart = start + 1;
+          textarea.selectionEnd = end + 1;
       }
     }
   }
